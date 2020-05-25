b1fc5bcfe1abc3998eefb59dc0fc742e3e0da0df
Adam Krueger <adamkru@gmail.com>
2020-05-25 11:36:38 -0500

    Updates to media player module.

--- PyBrowserDash/asgi.py
+++ PyBrowserDash/asgi.py
@@ -11,23 +11,22 @@ import os
 
 from django.core.asgi import get_asgi_application
 from PyBrowserDash.websocket import websocket_app
-from PyBrowserDash.foobar2k import start_listener, foobar2k_event_handler
+from PyBrowserDash.foobar2k import foobar2k_event_listener
 from asyncio import create_task
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PyBrowserDash.settings")
 
 django_app = get_asgi_application()
 ws_connections = set()
-foobar2k_listener_running = [False]
-# start_listener(ws_connections)
+foobar2k_listener = foobar2k_event_listener(ws_connections)
 
 
 async def application(scope, receive, send):
-    if not foobar2k_listener_running[0]:
-        create_task(foobar2k_event_handler(ws_connections))
-        foobar2k_listener_running[0] = True
+    if not foobar2k_listener.is_started():
+        create_task(foobar2k_listener.listen())
     scope["ws_connections"] = ws_connections
     scope_type = scope["type"]
+
     if scope_type == "http":
         await django_app(scope, receive, send)
     elif scope_type == "websocket":
--- PyBrowserDash/foobar2k.py
+++ PyBrowserDash/foobar2k.py
@@ -1,43 +1,73 @@
 import pyfoobeef
-from asyncio import create_task
 from json import dumps
 
 
-def send_foobar2k_updates(state, ws_connections):
-    output = {"playback_state": state.playback_state}
-    if state.active_item.has_columns():
-        col = state.active_item.columns
-        output["artist"] = col.artist
-        output["title"] = col.title
-        output["length"] = col.length
-        output["playback_position"] = state.active_item.position_mmss()
-    print(output)
-    for connection in ws_connections:
-        connection.send_msg(dumps(output))
+class foobar2k_event_listener():
+    """Listen for events from Foobar2000 and send updates to clients."""
 
+    def __init__(self, ws_connections):
+        self._listener = None
+        self._current_state = None
+        self._ws_connections = ws_connections
 
-async def foobar2k_event_handler(ws_connections):
-    listener = pyfoobeef.EventListener(
-        base_url="localhost",
-        port=6980,
-        active_item_column_map={
-            "%artist%": "artist",
-            "%title%": "title",
-            "%length%": "length",
-        },
-    )
+    def is_started(self):
+        """Return if listener is running or not."""
+        return self._listener is not None
 
-    def got_update(state):
-        send_foobar2k_updates(state, ws_connections)
+    async def listen(self):
+        """Start Event Listener."""
+        if self.is_started():
+            await self.disconnect()
+        self._listener = pyfoobeef.EventListener(
+            base_url="localhost",
+            port=6980,
+            active_item_column_map={
+                "%artist%": "artist",
+                "%title%": "title",
+                "%length%": "length",
+            },
+        )
 
-    # Add callback for player events.
-    listener.add_callback("player_state", got_update)
+        # Add callback for player events.
+        self._listener.add_callback("player_state", self.state_changed)
 
-    # Start listening for events from the player.
-    await listener.connect(reconnect_time=1)
+        # Start listening for events from the player.
+        await self._listener.connect(reconnect_time=1)
 
-    # await listener.disconnect()
+    def state_changed(self, state):
+        """Update the current player state and all clients."""
+        self._current_state = state
+        try:
+            self.update_all_clients()
+        except Exception as err:
+            print(err)
 
+    def update_all_clients(self):
+        """Send updates to clients."""
+        print("here")
+        if self._current_state is not None:
+            for connection in self._ws_connections:
+                connection.send_msg(self.make_update_str())
 
-def start_listener(ws_connections):
-    create_task(foobar2k_event_handler(ws_connections))
+    def make_update_str(self):
+        """Make a JSON string for websocket updates."""
+        if self._current_state is None:
+            return '{"playback_state": "disconnected"}'
+        state = self._current_state
+        output = {"playback_state": state.playback_state}
+        if state.active_item.has_columns():
+            col = state.active_item.columns
+            output["artist"] = col.artist
+            output["title"] = col.title
+            output["length"] = col.length
+            output["playback_position"] = state.active_item.position_mmss()
+
+        print(output)
+        return dumps({"music_player": output})
+
+    async def disconnect(self):
+        """Disconnect listener."""
+        if self._listener is not None:
+            await self._listener.disconnect()
+            self._listener = None
+            self._current_state = None
