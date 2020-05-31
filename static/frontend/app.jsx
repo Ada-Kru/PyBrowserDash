e88291e48be9c89a7f9eb094695dd6144fb782ec
Adam Krueger <adamkru@gmail.com>
2020-05-31 11:26:40 -0500

    Start work on status bar.
    Former-commit-id: 4c477564448ca0c92eee66c30e741b85570e9db4

--- static/frontend/app.jsx
+++ static/frontend/app.jsx
@@ -1,92 +1,135 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import StatusBar from "./components/StatusBar";
+
+const WS_DISCONNECTED = 0;
+const WS_CONNECTING = 1;
+const WS_CONNECTED = 2;
+const STATUS_NORMAL_STYLE = { color: "white" };
+const STATUS_ERROR_STYLE = { color: "red" };
 
 class App extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = { wsState: "disconnected" }
-        this.ws = null
+    this.state = {
+      wsState: WS_DISCONNECTED,
+      statusText: "Backend disconnected",
+      statusStyle: STATUS_ERROR_STYLE,
+      statusStats: "",
+      updatedStatusText: true,
+      updatedStatsOnly: false,
+    };
+    this.ws = null;
 
-        // Function maps for websocket events and internal events.
-        this._wsMsgMap = {
-            sys: this._onSystemInfoUpdate,
-            backend: this._onBackendUpdate,
-            music: this._onMusicPlayerUpdate,
-            new_msg: this._onNewMessages,
-            seen_msg: this._onSeenMessages,
-            clear_all_msg: this._onClearAllMessages,
+    // Function maps for websocket events and internal events.
+    this._wsMsgMap = {
+      sys: this._onSystemInfoUpdate,
+      backend: this._onBackendUpdate,
+      music: this._onMusicPlayerUpdate,
+      new_msg: this._onNewMessages,
+      seen_msg: this._onSeenMessages,
+      clear_all_msg: this._onClearAllMessages,
+    };
+  }
+
+  _setupWebsocket = () => {
+    this.ws = new WebSocket(
+      `ws://${window.location.hostname}:${window.location.port}/`
+    );
+    this.setState({
+      wsState: WS_CONNECTING,
+    });
+    this.setStatusText("Connecting...");
+
+    this.ws.onopen = (evt) => {
+      this.setState({
+        wsState: WS_CONNECTED,
+      });
+      this.setStatusText("Connected");
+    };
+
+    this.ws.onmessage = (evt) => {
+      let msg = JSON.parse(evt.data);
+      // console.log(msg);
+      for (let [type, data] of Object.entries(msg)) {
+        if (this._wsMsgMap.hasOwnProperty(type)) {
+          this._wsMsgMap[type](data);
         }
-    }
+      }
+    };
 
-    _setupWebsocket = () => {
-        this.ws = new WebSocket(
-            `ws://${window.location.hostname}:${window.location.port}/`
-        )
-        this.setState({ wsState: "connecting" })
+    this.ws.onclose = (evt) => {
+      this.setState({
+        wsState: WS_DISCONNECTED,
+      });
+      this.setStatusText("Backend disconnected", STATUS_ERROR_STYLE);
+      setTimeout(() => this._setupWebsocket(), 1000);
+    };
 
-        this.ws.onopen = evt => {
-            this.setState({ wsState: "connected" })
-        }
+    this.ws.error = (evt) => {
+      this.ws.close();
+    };
+  };
 
-        this.ws.onmessage = evt => {
-            let msg = JSON.parse(evt.data)
-            console.log(msg)
-            for (let [type, data] of Object.entries(msg)) {
-                if (this._wsMsgMap.hasOwnProperty(type)) {
-                    this._wsMsgMap[type](data)
-                }
-            }
-        }
+  _send = (data) => {
+    this.ws.send(JSON.stringify(data))
+  };
 
-        this.ws.onclose = (evt) => {
-            this.setState({ wsState: "disconnected" })
-            setTimeout(() => this._setupWebsocket(), 1000)
-        }
+  _onSystemInfoUpdate = (update) => {
+    this.setState({
+      statusStats: update,
+      updatedStatsOnly: !this.state.updatedStatsOnly,
+    });
+  };
 
-        this.ws.error = evt => {
-            this.ws.close()
-        }
-    }
+  _onBackendUpdate = (update) => {
+    console.log(update);
+  };
 
-    _send = data => {
-        this.ws.send(JSON.stringify(data))
-    }
+  _onMusicPlayerUpdate = (update) => {
+    console.log(update);
+  };
 
-    _onSystemInfoUpdate = update => {
-        console.log(update)
-    }
+  _onNewMessages = (update) => {
+    console.log(update);
+  };
 
-    _onBackendUpdate = update => {
-        console.log(update)
-    }
+  _onSeenMessages = (update) => {
+    console.log(update);
+  };
 
-    _onMusicPlayerUpdate = update => {
-        console.log(update)
-    }
+  _onClearAllMessages = (update) => {
+    console.log(update);
+  };
 
-    _onNewMessages = update => {
-        console.log(update)
-    }
+  componentDidMount = () => {
+    this._setupWebsocket();
+  };
 
-    _onSeenMessages = update => {
-        console.log(update)
-    }
+  setStatusText = (text, style = null) => {
+    this.setState({
+      updatedStatusText: !this.state.updatedStatusText,
+      statusText: text,
+      statusStyle: style == null ? STATUS_NORMAL_STYLE : style,
+    });
+  };
 
-    _onClearAllMessages = update => {
-        console.log(update)
-    }
-
-    componentDidMount() {
-        this._setupWebsocket()
-    }
-
-    render() {
-        return (
-            <div>
-                react loaded
-            </div>
-        )
-    }
+  render() {
+    let state = this.state;
+    return (
+      <div id="mainInteface">
+        <div id="modules">
+          <StatusBar
+            updatedStatusText={state.updatedStatusText}
+            updatedStatsOnly={state.updatedStatsOnly}
+            statusText={state.statusText}
+            statusStyle={state.statusStyle}
+            statusStats={state.statusStats}
+          ></StatusBar>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
---
+++ static/frontend/components/StatusBar.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class StatusBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      statusText: this.props.statusText,
+      statusStyle: this.props.statusStyle,
+      statusStats: this.props.statusStats,
+    };
+  }
+
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.updatedStatsOnly != this.props.updatedStatsOnly) {
+      this.setState({
+        statusStats: this.props.statusStats,
+      });
+    }
+    else if (prevProps.updatedStatusText != this.props.updatedStatusText) {
+      this.setState({
+        statusText: this.props.statusText,
+        statusStyle: this.props.statusStyle,
+        statusStats: this.props.statusStats,
+      });
+    }
+  };
+
+  render() {
+    let state = this.state;
+    return (
+      <div>
+        <span id="statusText" style={state.statusStyle}>
+          {state.statusText}
+        </span>
+        <span id="statusStats">{state.statusStats}</span>
+      </div>
+    );
+  }
+}
+
+export default StatusBar;
